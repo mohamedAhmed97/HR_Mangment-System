3d9fe67ef6d2206f6154f273021a83d723362fba
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-12-27 16:01:33 +0200

    create position model

--- contracts/models.py
+++ contracts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Department(models.Model):
-    department_name=models.CharField(max_length=55)
-    
+    department_name = models.CharField(max_length=55)
+
+
+class Position(models.Model):
+    position_name = models.CharField(max_length=55)
