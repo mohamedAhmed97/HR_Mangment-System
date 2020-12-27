72d88fbc29072d24ae02f144e51a120eadf8bc15
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-12-27 16:15:34 +0200

    create Contract mode l

--- contracts/models.py
+++ contracts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from employees.models import Employee
 # Create your models here.
 
 
@@ -9,3 +9,26 @@ class Department(models.Model):
 
 class Position(models.Model):
     position_name = models.CharField(max_length=55)
+
+
+class Contract(models.Model):
+    emp_id = models.ForeignKey(
+        Employee,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    dep_id = models.ForeignKey(
+        Department,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    pos_id = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
