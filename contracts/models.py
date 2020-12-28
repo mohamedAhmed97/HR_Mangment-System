c197657edc7444651c72e34dc688d9790bdf1169
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-12-28 10:38:17 +0200

    rollback to fix the wrong igonerd files in project

---
+++ contracts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from employees.models import Employee
+# Create your models here.
+
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=55)
+    department_desc = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    def __str__(self):
+        return self.department_name
+
+
+class Position(models.Model):
+    position_name = models.CharField(max_length=55)
+    def __str__(self):
+        return self.position_name
+
+class Contract(models.Model):
+    emp_id = models.ForeignKey(
+        Employee,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    dep_id = models.ForeignKey(
+        Department,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    pos_id = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
---
+++ leaves/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from employees.models import Employee
+# Create your models here.
+
+
+class LeaveMaster(models.Model):
+    leave_name = models.CharField(max_length=55)
+    leave_value = models.IntegerField()
+
+    def __str__(self):
+        return self.leave_name
+
+
+class EmployeeLevel(models.Model):
+    CONFIRM_CHOICE = (
+        ('w', 'waiting'),
+        ('a', 'accepted'),
+        ('r', 'rejected')
+    )
+    emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    leave_id = models.ForeignKey(LeaveMaster, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    confirm=models.CharField(max_length=1, choices=CONFIRM_CHOICE)
