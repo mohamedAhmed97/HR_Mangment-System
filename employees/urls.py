b7a1ce8ce11b8dd81f90e9bbcc81e78f43d719f2
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-12-29 15:53:47 +0200

    add Delete && update Employee

--- employees/urls.py
+++ employees/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 urlpatterns=[
     path('employees/',employees_list),
-    path('employees/add',create_employee)
+    path('employees/add',create_employee),
+    path('employees/delete/<emp_id>',delete_employee),
+    path('employees/edit/<emp_id>',edit_employee),
+
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Employee
 from .forms import EmployeeForm
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
 
 def employees_list(request):
     query_set = Employee.objects.all()
     context = {"employees": query_set}
     return render(request, 'employee_list.html', context)
 
+
 def create_employee(request):
-    form=EmployeeForm()
-    if request.method=="POST":
-        form =EmployeeForm(request.POST)
+    form = EmployeeForm()
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
         return HttpResponseRedirect('/employees')
-    return render(request,'create_employee.html',{'form':form})
+    return render(request, 'create_employee.html', {'form': form})
+
+
+def delete_employee(request, emp_id):
+    employee = Employee.objects.get(id=emp_id)
+    employee.delete()
+    return HttpResponseRedirect('/employees')
+
+
+def edit_employee(request, emp_id):
+    employee = get_object_or_404(Employee, id=emp_id)
+    if request.method == "POST":
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/employees')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'create_employee.html', {'form': form})
